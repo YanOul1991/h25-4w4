6fc64243939d33e2e27feee2e6620450fa453761
Yanis Oulmane <152992744+YanOul1991@users.noreply.github.com>
2025-03-17 18:05:20 -0400

    s8c1.5 - Changement de la structure du gabarit carte.php et début du CSS pour les cartes.

--- gabarits/carte.php
+++ gabarits/carte.php
@@ -1,18 +1,20 @@
-<article class="carte carte--grande">
-    <div class="carte__contenu">
+<article class="carte">
+    <div class="carte__image">
         <?php 
-            if (has_post_thumbnail()) {
-                the_post_thumbnail('thumbnail'); 
-            }
+        if (has_post_thumbnail()) {
+            the_post_thumbnail('thumbnail'); 
+        }
         ?> 
-        <h4 class="carte__titre"><?php the_title();?></h4>  
-        <p class="carte__description"></p><?php echo wp_trim_words(get_the_content(), 20, "...");?> 
-        <a  class="carte__bouton carte__bouton--actif" href="<?php the_permalink();?> ">Suite ...</a>
-        <?php the_category();?> 
+    </div>
 
+    <h4 class="carte__titre"><?php the_title();?></h4>  
+    <p class="carte__description"><?php echo wp_trim_words(get_the_content(), 10, "...");?><a class="carte__bouton carte__bouton--actif" href="<?php the_permalink();?> "> Suite ...</a></p>
+    
+    <?php the_category()?> 
+
+    <div class="carte_température">
         <p>Température maximum : <?php echo rand(20, 40); # the_field('temperature_maximum'); ?> °C</p>
         <p>Température minimum : <?php echo rand(20, 40); # the_field('temperature_minimum'); ?> °C</p>
         <p>Température maximum : <?php echo rand(20, 40); # the_field('temperature_maximum'); ?> °C</p>
-        <!-- <button class="carte__bouton carte__bouton--actif">Suite</button> -->
     </div>
 </article>
--- header.php
+++ header.php
@@ -4,13 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prototype page d'accueil</title>
-    <!-- <link rel="stylesheet" href="normalize.css"> -->
-    <!-- <link rel="stylesheet" href="style.css"> -->
     <?php wp_head();?>
 </head>
 <body>
-
-
     <!-- HEADER -->
     <header>
         <div class="entete">
