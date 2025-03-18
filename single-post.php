4d6f11b6331df8bca29e40974fe3dce98d7885dc
Yanis Oulmane <152992744+YanOul1991@users.noreply.github.com>
2025-03-18 13:46:02 -0400

    Structurage de single-post.php et style CSS responsive terminé pour les postes.

--- single-post.php
+++ single-post.php
@@ -1,24 +1,30 @@
-<?php get_header();?>   
-<h1> ---------------------------- Single-post ---------------------------- </h1>
-    <section class="populaire">
-        <div class="global">
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <article>
-                    <?php 
-                        if (has_post_thumbnail()) {
-                            the_post_thumbnail('large'); 
-                        }
-                    ?> 
-                    <h2><?php the_title(); ?></h2>
-                    <h3><?php the_category();?></h3>
-                    <div><?php the_content(); ?></div>
-                    <p>Température maximum  <?php the_field('temperature_maximum'); ?> °C</p>
-                    <p>Température minimum  <?php the_field('temperature_minimum'); ?> °C</p>
-                    <p>Température moyenne  <?php the_field('temperature_moyenne'); ?> °C</p>
-                </article>
-            <?php endwhile; endif; ?>
-        </div>
-    </section>
-    <?php wp_footer();?> 
+<?php get_header(); ?>
+<main class="singlepost global">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <section class="singlepost__main">
+                <h1 class="singlepost__main__titre"><?php the_title(); ?></h1>
+                <?php if (has_post_thumbnail()) : ?>
+                    <div class="singlepost__main__image">
+                        <?php the_post_thumbnail('medium'); ?>
+                    </div>
+                <?php endif; ?>
+            </section>
+            <section class="singlepost__infos">
+                <?php the_category(); ?>
+                <p class="singlepost__infos__description"><?php echo strip_tags(get_the_content()); ?></p>
+                <div class="singlepost__infos__temperatures">
+                    <p>Température maximum : <?php the_field('temperature_maximum');?> °C</p>
+                    <p>Température minimum : <?php the_field('temperature_minimum');?> °C</p>
+                    <p>Température moyenne : <?php the_field('temperature_moyenne');?> °C</p>
+                </div>
+            </section>
+    <?php endwhile;
+    endif; ?>
+</main>
+<?php
+get_footer();
+wp_footer();
+?>
 </body>
+
 </html>
