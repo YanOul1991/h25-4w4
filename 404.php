762a96d251de8076f5fb468320b8ff1b1876e819
Yanis Oulmane <152992744+YanOul1991@users.noreply.github.com>
2025-03-24 14:33:04 -0400

    s9c1 - Ajout de 404.php

---
+++ 404.php
@@ -0,0 +1,13 @@
+
+<main class="notfound" style="height: 100%; display: flex; flex-flow: column nowrap; justify-content: space-between";>
+    <?php get_header(); ?>
+    <div class="notfound__infos">
+        <h1 class="notfound__titre">ERREUR 404</h1>
+        <h1 class="notfound__infos__description">La page que vous cherchez n'éxiste pas.</h1>
+    </div>
+    <div class="notfound__image">
+        <img src="" alt="Image 404 not found">
+    </div>
+    <?php get_footer(); ?>
+    <?php wp_footer(); ?>
+</main>
