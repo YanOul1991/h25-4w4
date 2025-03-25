19efb4b11f5a2b67bbeea65a4fde5ccf14d641b5
Yanis Oulmane <152992744+YanOul1991@users.noreply.github.com>
2025-03-25 17:36:02 -0400

    s9c1 - Commencement du style css pour la page 404.php

--- 404.php
+++ 404.php
@@ -1,15 +1,13 @@
 <?php get_header(); ?>
-<main class="notfound"
-    style="height: max-content; display: flex; flex-flow: column nowrap; justify-content: space-between" ;>
+<main class="notfound global">
     <!-- ------------- -->
     <div class="notfound__infos">
-        <h1 class="notfound__titre"><?php echo get_theme_mod("notfound_titre", ""); ?></h1>
+        <h1 class="notfound__infos__titre"><?php echo get_theme_mod("notfound_titre", ""); ?></h1>
         <h1 class="notfound__infos__description"><?php echo get_theme_mod("notfound_description", ""); ?></h1>
     </div>
     <!-- ------------- -->
     <div class="notfound__image">
-        <img src="<?php echo get_theme_mod("notfound_image", ""); ?>" alt="Image 404 not found"
-            style="width: 20vw;">
+        <img src="<?php echo get_theme_mod("notfound_image", ""); ?>" alt="Image 404 not found">
     </div>
     <!-- ------------- -->
     <div class="notfound__options">
@@ -19,9 +17,12 @@
             'container_class' => 'notfound__menu'
         )); ?>
     </div>
+    <div class="notfound__icons">
+        <?php get_template_part("gabarits/icones");?> 
+    </div>
     <div class="notfound__search">
         <?php get_search_form(); ?>
     </div>
-    <?php get_footer(); ?>
-    <?php wp_footer(); ?>
 </main>
+<?php get_footer(); ?>
+<?php wp_footer(); ?>
--- gabarits/icones.php
+++ gabarits/icones.php
@@ -1,7 +1,4 @@
-<?php 
-    $hero_icon_couleur = get_theme_mod('hero_color', '');
-?> 
-
+<?php $hero_icon_couleur = get_theme_mod('hero_color', '');?> 
 
 <div class="hero__icone">
     <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=<?php echo str_replace("#", "", $hero_icon_couleur); ?>" width="40" height="40">
