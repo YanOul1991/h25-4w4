ad7df0b570e01c4df6f87584ed9ed5a0c06ca4b2
Yanis Oulmane <152992744+YanOul1991@users.noreply.github.com>
2025-03-25 17:57:47 -0400

    s9c1 - CSS pour 404.php terminé.

--- gabarits/icones.php
+++ gabarits/icones.php
@@ -1,7 +1,10 @@
-<?php $hero_icon_couleur = get_theme_mod('hero_color', '');?> 
+<?php 
+    $hero_icon_couleur = get_theme_mod('hero_color', '');
+    $couleur = str_replace("#", "", $hero_icon_couleur);
+?> 
 
 <div class="hero__icone">
-    <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=<?php echo str_replace("#", "", $hero_icon_couleur); ?>" width="40" height="40">
-    <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=<?php echo str_replace("#", "", $hero_icon_couleur); ?>" width="40" height="40">
-    <img src="https://s2.svgbox.net/social.svg?ic=wordpress&color=<?php echo str_replace("#", "", $hero_icon_couleur); ?>" width="40" height="40">
+    <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=<?php echo is_404() ? "00000" : $couleur; ?>" width="40" height="40">
+    <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=<?php echo is_404() ? "00000" : $couleur; ?>" width="40" height="40">
+    <img src="https://s2.svgbox.net/social.svg?ic=wordpress&color=<?php echo is_404() ? "00000" : $couleur; ?>" width="40" height="40">
 </div>
