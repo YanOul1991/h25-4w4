0d4a97f2b2d27a402bf3b02c6e1100afab2fd5f4
Yanis Oulmane <152992744+YanOul1991@users.noreply.github.com>
2025-03-31 14:44:18 -0400

    Exament intra - maquette 404.php terminé.

--- 404.php
+++ 404.php
@@ -4,7 +4,9 @@
         <h1 class="notfound__infos__titre" style="color: <?php echo get_theme_mod("notfound_color", "") ?>;"><?php echo get_theme_mod("notfound_titre", ""); ?></h1>
         <h1 class="notfound__infos__description" style="color: <?php echo get_theme_mod("notfound_color", "") ?>;"><?php echo get_theme_mod("notfound_description", ""); ?></h1>
     </div>
-    <div class="notfound__retour"><a href="index.php">Retour à l'accueil</a></div>
+    <div class="notfound__retour">
+        <a href="index.php">Retour à l'accueil</a>
+    </div>
     <div class="notfound__options">
         <?php wp_nav_menu(array(
             'menu' => 'notfound',
