13eacd3fc7c15a3291383cbf53b7ef0d06c55eab
Yanis Oulmane <152992744+YanOul1991@users.noreply.github.com>
2025-05-23 10:55:31 -0400

    Stylization de la page des destinations de pays et page evenement. Travail termine.

--- template-evenement.php
+++ template-evenement.php
@@ -4,26 +4,14 @@ Template Name: Événement
 */
 get_header();
 ?>
-<section class="populaire">
-    <div class="global">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <article>
-                    <h2><?php the_title(); ?></h2>
-                    <div><?php the_content(); ?></div>
-                </article>
-                <p>Date de l'événement :
-                    <?php the_field('date_evenement'); ?></p>
-                <p>Lieu : <?php the_field('lieu_evenement'); ?></p>
-                <div>
-                    <?php the_field('description_evenement'); ?>
-                </div>
-        <?php endwhile;
-        endif; ?>
-        <section class="destination">
-            <?php categories_liste("destination"); ?>
-            <h2 class="destination__titre">Articles de la catégorie</h2>
-            <div class="destination__list"></div>
-        </section>
-    </div>
+<section class="evenement global" style="display: flex; flex-direction:column; gap: 25px;">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <h2 class="evenement__titre"><?php the_title(); ?></h2>  
+            <div class="evenement__content"><?php the_content(); ?></div>
+            <p class="evenement__date" style="font-size: 3rem; font-weight: bolder;">Date de l'événement : <?php the_field('date_evenement'); ?></p>
+            <p class="evenement__lieu" style="font-size: 2rem; font-weight: bolder;">Lieu : <?php the_field('lieu_evenement'); ?></p>
+            <div class="evenement__description" style="font-size: 2rem;"><?php the_field('description_evenement'); ?></div>
+    <?php endwhile;
+    endif; ?>
 </section>
 <?php get_footer(); ?>
--- template-pays.php
+++ template-pays.php
@@ -4,16 +4,14 @@ Template Name: Pays
 */
 get_header();
 ?>
-<section class="pays">
-    <div class="global">
-        <h2 class="pays__titre"><?php the_title(); ?></h2>
-        <div class="pays__intro"><?php the_content(); ?></div>
-        <?php genere_vague("#6cefa0") ?>
-        <section class="pays__main" style="display: flex; flex-flow: row wrap;">
-            <?php genererListePays(array("France", "États-Unis", "Canada", "Argentine", "Chili", "Belgique", "Maroc", "Mexique", "Japon", "Italie", "Islande", "Chine", "Grèce", "Suisse")) ?>
-            <div class="pays__main__titre"></div>
-            <div class="pays__liste"></div>
-        </section>
-    </div>
+<section class="pays global">
+    <h2 class="pays__titre" style="font-size: 4rem; font-weight:bolder;"><?php the_title(); ?></h2>
+    <?php the_content(); ?>
+    <?php genere_vague("#6cefa0") ?>
+    <section class="pays__main">
+        <?php genererListePays(array("France", "États-Unis", "Canada", "Argentine", "Chili", "Belgique", "Maroc", "Mexique", "Japon", "Italie", "Islande", "Chine", "Grèce", "Suisse")) ?>
+        <div class="pays__main__titre"></div>
+        <div class="pays__liste"></div>
+    </section>
 </section>
 <?php get_footer(); ?>
